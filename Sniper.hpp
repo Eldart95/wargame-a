1457ef9701e50775e95a9027ad41f3f70b47a959
Eldar Takach <eldartakach@gmail.com>
2020-06-06 09:51:32 +0300

    beta 1.01

--- Sniper.hpp
+++ Sniper.hpp
@@ -4,7 +4,19 @@
 namespace WarGame{
     class Sniper : public Soldier{
     public:
-        Sniper(int pn) : Soldier(100,pn,50){}
+        Sniper(int pn) : Soldier(100,pn,50,100){}
+
+        void fire(std::pair<int,int> source,std::vector<std::vector<Soldier*>>& board) override{
+            std::pair<int,int> target = this->highest_hp(source,board);
+            if(target.first==-1 && target.second==-1) return; // if there is'nt such a player - return
+
+            board[target.first][target.second]->hp-=board[source.first][source.second]->dmg;
+            if(board[target.first][target.second]->hp<=0){
+                delete(board[target.first][target.second]);
+                board[target.first][target.second]=nullptr;
+            }
+
+        }        
 
     };
 }
--- Soldier.hpp
+++ Soldier.hpp
@@ -1,5 +1,5 @@
-// #ifndef SOLDIER_H
-// #define SOLDIRT_H
+#include <vector>
+#include <cmath>
 #pragma once
 
 namespace WarGame{
@@ -8,10 +8,62 @@ namespace WarGame{
         int player;
         int hp;
         int dmg;
+        int max_hp;
 
-        Soldier(int hp, int p,int dmg): hp(hp), player(p) , dmg(dmg) {}
+        virtual ~Soldier() {}
 
+
+
+        Soldier(int hp, int p,int dmg,int max_hp): hp(hp), player(p) , dmg(dmg) , max_hp(max_hp) {}
+
+        virtual void fire(std::pair<int,int> source,std::vector<std::vector<Soldier*>>& board)=0;
+        //naive search for closest player
+        std::pair<int,int> closest_target(std::pair<int,int> source,const std::vector<std::vector<Soldier*>>& board){
+            std::pair<int,int> target = {-1,-1};
+
+            double dist;
+            double min=999;
+            int p;
+            board[source.first][source.second]->player == 1 ? p=2 : p=1;
+
+            for(int i=0;i<board.size();i++){
+                for(int j=0;j<board[i].size();j++){
+                    if(board[i][j]!=nullptr && board[i][j]->player==p){
+                        dist=sqrt(pow(source.first-i,2)+pow(source.second-j,2)); // distant formula : sqrt(a^2+b^2)
+                        if(dist<min) {
+                            min=dist;
+                            target = {i,j};
+                        }
+                    }
+                }
+        
+            } 
+            return target;  
+            
+        }
+
+        //naive search for player with highest HP.
+        std::pair<int,int> highest_hp(std::pair<int,int> source,const std::vector<std::vector<Soldier*>>& board){
+            std::pair<int,int> target = {-1,-1};
+            double hp;
+            double max=0;
+            int p;
+            board[source.first][source.second]->player == 1 ? p=2 : p=1;
+
+            for(int i=0;i<board.size();i++){
+                for(int j=0;j<board[i].size();j++){
+                    if(board[i][j]!=nullptr && board[i][j]->player==p){
+                        hp=board[i][j]->hp; // distant formula : sqrt(a^2+b^2)
+                        if(hp>max) {
+                            max=hp;
+                            target = {i,j};
+                        }
+                    }
+                }
+        
+            } 
+            return target;  
+            
+        }        
     };
-
 }
-// #endif
